a89cef5327510c199b35a72c4366e9ef68f0125a
vasilisa19901@gmail.com <oloktionov1985@gmail.com>
2019-05-27 20:24:08 +0300

    mod to ion html and ts files add-com and info-com

--- src/app/+main/add-company/add-company.page.ts
+++ src/app/+main/add-company/add-company.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {CompaniesService} from '../../shared/services/companies/companies.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-add-company',
@@ -7,10 +10,29 @@ import { NavController, NavParams } from 'ionic-angular';
   styleUrls: ['./add-company.page.scss'],
 })
 export class AddCompanyPage implements OnInit {
-
-  constructor() { }
+  protected addCompanyForm: FormGroup;
+  constructor(private formBuilder: FormBuilder, private companyService: CompaniesService, private router: Router) {
+    this._buildForm();
+  }
 
   ngOnInit() {
   }
+  get f() { return this.addCompanyForm.controls; }
 
+  private addCompany() {
+    const {name, city, address} = this.addCompanyForm.value;
+    //temprorary solution
+    const id = Math.floor(Math.random() * Math.floor(100));
+    this.companyService.addCompany({id, name, city, address, latitude: null, longitude: null, img: null});
+    //in real situation we will be waiting the response from server and after show some message for example
+    this.router.navigate(['main']);
+  }
+
+  protected _buildForm() {
+    this.addCompanyForm = this.formBuilder.group({
+      name: ['', [Validators.required]],
+      city: ['', [Validators.required]],
+      address: ['', [Validators.required]],
+    });
+}
 }
